4add6ee07b60e6ac2beca48f477c8525cfbde967
liadahao <liadahao@gmail.com>
2015-04-10 16:47:22 +0800

    修订

--- BiTree.java
+++ BiTree.java
@@ -2,23 +2,8 @@ package com.datastructure;
 
 public class BiTree<T> {
 
-	private BiTree left;
-	private BiTree right;
-	private BiTree root;
-	private T data;
-
-	public BiTree(Object data, BiTree left, BiTree right) {
-		this.data = (T) data;
-		this.left = left;
-		this.right = right;
-	}
-
-	public BiTree(T data) {
-		this.data = data;
-	}
-
-	public void pre(BiTree t) {
-		java.util.Stack<BiTree> s = new java.util.Stack<BiTree>();
+	public void PreOrderTraverse(BiTreeNode<T> t) {
+		java.util.Stack<BiTreeNode> s = new java.util.Stack<BiTreeNode>();
 		s.push(t);
 		while (!s.isEmpty()) {
 			t = s.pop();
@@ -31,8 +16,8 @@ public class BiTree<T> {
 		}
 	}
 
-	public void in(BiTree t) {
-		java.util.Stack<BiTree> s = new java.util.Stack<BiTree>();
+	public void InOrderTraverse(BiTreeNode<T> t) {
+		java.util.Stack<BiTreeNode> s = new java.util.Stack<BiTreeNode>();
 		while (t != null || !s.isEmpty()) {
 			while (t != null) {
 				s.push(t);
@@ -43,28 +28,47 @@ public class BiTree<T> {
 				System.out.println(t.data);
 				t = t.right;
 			}
+
 		}
 	}
 
-	public void post(BiTree t) {
-		java.util.Stack<BiTree> s = new java.util.Stack<BiTree>();
-		BiTree rnode = null;
+	public void PostOrderTraverse(BiTreeNode<T> t) {
+		java.util.Stack<BiTreeNode> s = new java.util.Stack<BiTreeNode>();
+		BiTreeNode<T> rnode = null;
 		while (t != null || !s.isEmpty()) {
 			while (t.left != null) {
 				s.push(t);
 				t = t.left;
 			}
-			while(t!=null&&t.right==null||t.right==rnode){
+			while (t != null && t.right == null || t.right == rnode) {
 				System.out.println(t.data);
-				rnode=t;
-				if (s.isEmpty()){  
-                    System.out.println();  
-                    return;  
-                } 
-				t=s.pop();
+				rnode = t;
+				if (s.isEmpty()) {
+					System.out.println();
+					return;
+				}
+				t = s.pop();
 			}
 			s.push(t);
-			t=t.right;
+			t = t.right;
 		}
 	}
+}
+
+class BiTreeNode<T> {
+
+	public BiTreeNode(T data) {
+		this.data = data;
+	}
+
+	public BiTreeNode(T data, BiTreeNode<T> left, BiTreeNode<T> right) {
+		this.left = left;
+		this.right = right;
+		this.data = data;
+	}
+
+	BiTreeNode<T> left;
+	BiTreeNode<T> right;
+	BiTreeNode<T> root;
+	T data;
 }
--- BiTreeTest.java
+++ BiTreeTest.java
@@ -7,13 +7,20 @@ import org.junit.Test;
 public class BiTreeTest {
 
 	@Test
-	public final void test() {			
-		BiTree c=new BiTree("a");
-		BiTree d=new BiTree("b");
-		BiTree a=new BiTree("*",c,d);
-		BiTree b=new BiTree("c");
-		BiTree t=new BiTree("-",a,b);
-		t.pre(t);
+	public final void test() {
+		BiTreeNode c = new BiTreeNode("a");
+		BiTreeNode e = new BiTreeNode("b");
+		BiTreeNode g = new BiTreeNode("c");
+		BiTreeNode k = new BiTreeNode("e");
+		BiTreeNode l = new BiTreeNode("f");
+		BiTreeNode h = new BiTreeNode("d");
+		BiTreeNode f = new BiTreeNode("-", g, h);
+		BiTreeNode d = new BiTreeNode("*", e, f);
+		BiTreeNode a = new BiTreeNode("+", c, d);
+		BiTreeNode b = new BiTreeNode("/", k, l);
+		BiTreeNode t = new BiTreeNode("-", a, b);
+		BiTree z = new BiTree();
+		z.InOrderTraverse(t);
 	}
 
 }
